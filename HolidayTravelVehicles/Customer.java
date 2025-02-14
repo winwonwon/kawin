9e48f9dc53c6e0a36b1af60cbb7a1f98bcc5a822
winwonwon <52631953+winwonwon@users.noreply.github.com>
2025-02-14 15:51:12 +0700

    Update Customer.java

--- HolidayTravelVehicles/Customer.java
+++ HolidayTravelVehicles/Customer.java
@@ -1,1 +1,16 @@
+public class Customer extends Person {
+    private int customerID;
 
+    public Customer(String name, String address, String phoneNumber, int customerID) {
+        super(name, address, phoneNumber);
+        this.customerID = customerID;
+    }
+
+    public void addOptions() {
+        System.out.println("Adding options...");
+    }
+
+    public void tradeVehicle() {
+        System.out.println("Trading vehicle...");
+    }
+}

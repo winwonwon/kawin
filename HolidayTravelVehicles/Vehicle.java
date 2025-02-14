6bdadb35cf5b5895185d3212fb27b85f59d7d24d
winwonwon <52631953+winwonwon@users.noreply.github.com>
2025-02-14 16:23:40 +0700

    Update Vehicle.java

--- HolidayTravelVehicles/Vehicle.java
+++ HolidayTravelVehicles/Vehicle.java
@@ -10,4 +10,12 @@ public class Vehicle {
         this.model = model;
         this.year = year;
     }
+
+    public void displayInfo() {
+        System.out.println("Vehicle Info:");
+        System.out.println("Serial Number: " + serialNumber);
+        System.out.println("Manufacturer: " + manufacturer);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
 }

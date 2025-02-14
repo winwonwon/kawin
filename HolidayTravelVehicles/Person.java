f920a39e6b22eac797d353376b96453412ba9a25
winwonwon <52631953+winwonwon@users.noreply.github.com>
2025-02-14 15:51:42 +0700

    Create Person.java

---
+++ HolidayTravelVehicles/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    protected String name;
+    protected String address;
+    protected String phoneNumber;
+
+    public Person(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+}
